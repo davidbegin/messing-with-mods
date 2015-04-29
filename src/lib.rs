b16dc7283ba121a0c0564a5f526d0892a6dac373
David Begin <davidmichaelbe@gmail.com>
2015-04-28 22:03:42 -0700

    A detour, a couple defeats, and a marginal victory
    printing colors from a function, but now that its done, I realize I want to use impl and try and monkey patch strings or numbers to be able
    to print their color, or something wacky like that. But thats probably
    too hard for me right now. Or no probable in rust….or not!

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,27 @@
-// some mod makes something like a module in ruby I'm guessing
-// so lets run with that
-
-// I also am going to steal the code from here
-// https://doc.rust-lang.org/term/term/index.html
-//
-// and try and understand it
-
-
-// Well Well Well
-// Good ole unstable Rust!
-//
-// [ERROR FOR POSTERITY]
-// help: add #![feature(rustc_private)] to the crate attributes to enable
-// error: use of unstable library feature 'rustc_private': use the crates.io `term`
-//
-// adding this is admitting defeat
-//
-// the battle is lost
-//
-// ...but not the war
 #![feature(rustc_private)]
+#![feature(core)]
 
 extern crate term;
 use std::io::prelude::*;
 
+pub mod fancy_printer_exclamation_point {
+    extern crate term;
+    use std::io::prelude::*;
+
+    pub fn fancy_print(string: &'static str) {
+        let mut t = term::stdout().unwrap();
+        t.fg(term::color::BLACK).unwrap();
+        t.bg(term::color::BRIGHT_MAGENTA).unwrap();
+        (write!(t, "{}", string)).unwrap();
+        t.reset().unwrap();
+    }
+}
+
 fn main() {
+    use fancy_printer_exclamation_point;
     print_title();
 
-    let mut somehow_this_is_stdout_and_it_has_to_mutable_eh = term::stdout().unwrap();
-
-    // I'm guessing fg is foreground
-    // so I almost just want to try out bg first
-    //
-    // ...but alas I will wait
-    somehow_this_is_stdout_and_it_has_to_mutable_eh.fg(term::color::GREEN).unwrap();
-    somehow_this_is_stdout_and_it_has_to_mutable_eh.bg(term::color::BLUE).unwrap();
-    (write!(somehow_this_is_stdout_and_it_has_to_mutable_eh, "hello, ")).unwrap();
-
-    somehow_this_is_stdout_and_it_has_to_mutable_eh.fg(term::color::RED).unwrap();
-    somehow_this_is_stdout_and_it_has_to_mutable_eh.bg(term::color::YELLOW).unwrap();
-    (write!(somehow_this_is_stdout_and_it_has_to_mutable_eh, "world!")).unwrap();
-
-    somehow_this_is_stdout_and_it_has_to_mutable_eh.reset().unwrap();
-
+    fancy_printer_exclamation_point::fancy_print("A Fancy Sentence; hence the the semi-colon.");
     seperator();
 }
 
@@ -52,7 +31,7 @@ fn print_title() {
 }
 
 fn seperator() {
-    println!("\n")
+    println!("\n");
 }
 
 #[test]
