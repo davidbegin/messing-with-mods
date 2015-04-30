ac7a6206f54e791a72c872ee74ced189ca71b094
David Begin <davidmichaelbe@gmail.com>
2015-04-29 18:41:33 -0700

    add another spec
    …just for an excuse to add a a tests module

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,17 @@ fn main() {
     print_stuff::seperator();
 }
 
-#[test]
-fn test_math_in_your_face_returns_255() {
-    assert_eq!(ti_83::math_in_your_face(), 255);
+#[cfg(test)]
+mod tests {
+    use super::ti_83;
+
+    #[test]
+    fn test_math_in_your_face_returns_255() {
+        assert_eq!(ti_83::math_in_your_face(), 255);
+    }
+
+    #[test]
+    fn test_add_two_numbers_works() {
+        assert_eq!(ti_83::add_two_numbers(9, 7), 16);
+    }
 }
--- src/ti_83/mod.rs
+++ src/ti_83/mod.rs
@@ -1,3 +1,7 @@
 pub fn math_in_your_face() -> i8 {
     255
 }
+
+pub fn add_two_numbers(x: i8, y: i8) -> i8 {
+    x + y
+}
